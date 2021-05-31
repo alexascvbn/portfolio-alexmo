c986f43c211ed9c69bcdca8045baa736918db39f
alexascvbn <alexascvbn@gmail.com>
2021-05-31 16:36:42 +0800

    Services Completed

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Particles from "react-particles-js";
 import Header from "./components/Header";
 import Navbar from "./components/Navbar";
 import AboutMe from "./components/AboutMe";
+import Services from "./components/Services";
 
 function App() {
   return (
@@ -33,6 +34,7 @@ function App() {
       <Navbar />
       <Header />
       <AboutMe />
+      <Services />
     </>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -11,7 +11,7 @@ const Header = () => {
           className="typed-text"
           strings={[
             "Web Development",
-            "Mobile App",
+            "Mobile Application",
             "Google Ads",
             "SEO engine",
           ]}
