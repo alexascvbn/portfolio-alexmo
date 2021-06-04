872f04716e783aeb7d608eb3ca35f2ae91981b60
alexascvbn <alexascvbn@gmail.com>
2021-06-04 15:28:46 +0800

    Portfolio Component Completed

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Navbar from "./components/Navbar";
 import AboutMe from "./components/AboutMe";
 import Services from "./components/Services";
 import Experience from "./components/Experience";
+import Portfolio from "./components/Portfolio";
 
 function App() {
   return (
@@ -37,6 +38,7 @@ function App() {
       <AboutMe />
       <Services />
       <Experience />
+      <Portfolio />b
     </>
   );
 }
