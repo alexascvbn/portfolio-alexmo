52de67764f15c0f3dcf822704e4d9fa7759b6309
alexascvbn <alexascvbn@gmail.com>
2021-06-04 16:34:44 +0800

    Testimonials Component Completed

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import AboutMe from "./components/AboutMe";
 import Services from "./components/Services";
 import Experience from "./components/Experience";
 import Portfolio from "./components/Portfolio";
+import Testimonials from "./components/Testimonials";
 
 function App() {
   return (
@@ -38,7 +39,8 @@ function App() {
       <AboutMe />
       <Services />
       <Experience />
-      <Portfolio />b
+      <Portfolio />
+      <Testimonials />
     </>
   );
 }
